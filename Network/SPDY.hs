534de970b3f20912f83b27acb4d8b3cf1ce9f611
Lennart Kolmodin <kolmodin@gmail.com>
2012-01-27 13:42:19 +0400

    Run serverside in ResourceT from conduit.

--- Network/SPDY.hs
+++ Network/SPDY.hs
@@ -8,6 +8,9 @@ import Data.Binary.Bits
 import Data.Binary.Bits.Get
 import Data.Binary.Bits.Put
 
+import Data.Conduit hiding (Done)
+import Control.Monad.IO.Class (liftIO)
+
 import Network.SPDY.Frame
 
 import Network.Socket hiding ( recv )
@@ -22,7 +25,7 @@ import qualified Data.ByteString as S
 
 import Data.Monoid
 
-server :: (Socket -> SockAddr -> IO ()) -> String -> IO ()
+server :: (Socket -> SockAddr -> ResourceT IO ()) -> String -> IO ()
 server handler port = withSocketsDo $ do
   addrinfos <- getAddrInfo
                  (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
@@ -33,12 +36,11 @@ server handler port = withSocketsDo $ do
   listen sock 1
   let loop = do
         (conn, sockaddr) <- accept sock
-        forkIO $ handler conn sockaddr
+        forkIO $ runResourceT $ handler conn sockaddr
         loop
   loop
 
-type SessionHandler = Socket -> SockAddr -> IO ()
-type FrameHandler = SessionState -> Frame -> IO SessionState
+type FrameHandler m = SessionState -> Frame -> ResourceT m SessionState
 
 data SessionState = SessionState
   { sessionStateSendQueue :: TVar [Frame]  -- TODO(kolmodin): use a priority queue
@@ -49,29 +51,34 @@ initSession = do
   queue <- newTVarIO []
   return $ SessionState queue
 
-frameHandler :: FrameHandler
+frameHandler :: ResourceIO m => FrameHandler m
 frameHandler state frame = do
   case frame of
     SynStreamControlFrame flags sId assId pri nvh -> do
-      print frame
+      liftIO $ print frame
       createSession state sId pri []
       let syn_reply = SynReplyControlFrame 0 sId S.empty
       -- enqueueFrame state syn_reply
       return state
+    RstStreamControlFrame flags sId status -> do
+      liftIO $ putStrLn "RstStream... we're screwed."
+      liftIO $ print frame
+      -- TODO: remove all knowledge of this stream. empty send buffer.
+      return state
     PingControlFrame pingId -> do
-      print frame
+      liftIO $ print frame
       enqueueFrame state (PingControlFrame pingId)
       return state
 
-enqueueFrame :: SessionState -> Frame -> IO ()
-enqueueFrame SessionState { sessionStateSendQueue = queue } frame = do
-  atomically $ do
+enqueueFrame :: ResourceIO m => SessionState -> Frame -> ResourceT m ()
+enqueueFrame SessionState { sessionStateSendQueue = queue } frame =
+  liftIO $ atomically $ do
     q <- readTVar queue
     writeTVar queue (frame:q)
 
-createSession :: SessionState -> Word32 -> Word8 -> NameValueHeaderBlock -> IO ()
+createSession :: ResourceIO m => SessionState -> Word32 -> Word8 -> NameValueHeaderBlock -> ResourceT m ()
 createSession state sId pri nvh = do
-  putStrLn $ "Creating stream context, id = " ++ show sId
+  liftIO $ putStrLn $ "Creating stream context, id = " ++ show sId
 
 sender :: Socket -> TVar [Frame] -> IO ()
 sender socket queue = go
@@ -91,19 +98,19 @@ sender socket queue = go
              return frame
         [] -> retry
 
-sessionHandler :: FrameHandler -> SessionHandler
+sessionHandler :: ResourceIO m => FrameHandler m -> Socket -> SockAddr -> ResourceT m ()
 sessionHandler handler conn sockaddr = do
-  init <- initSession
-  forkIO $ sender conn (sessionStateSendQueue init)
+  init <- liftIO $ initSession
+  liftIO $ forkIO $ sender conn (sessionStateSendQueue init)
   go init (runGetPartial (runBitGet getFrame))
   where
   go s r =
     case r of
       Fail _ _ msg -> error msg
       Partial f -> do
-        raw <- NS.recv conn (16 * 1024)
+        raw <- liftIO $ NS.recv conn (16 * 1024)
         go s (f $ Just raw)
       Done rest pos frame -> do
-        putStrLn "Parsed frame."
+        liftIO $ putStrLn "Parsed frame."
         s' <- handler s frame
         go s' (runGetPartial (runBitGet getFrame) `feed` rest)
